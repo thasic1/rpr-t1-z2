9c945817e4c2e95df2bc4bec70c67092fd4ae70a
thasic1 <thasic1@etf.unsa.ba>
2020-10-26 18:31:02 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+
+
+public class Main{
+    public static int sumaCifara(int n){
+        int suma=0;
+        while(n!=0){
+            suma=suma+n%10;
+            n=n/10;
+        }
+        return suma;
+    }
+    public static void main(String[] args){
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite n: ");
+        int n = ulaz.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i%sumaCifara(i)==0)
+                System.out.println(i);
+        }
+    }
+}
